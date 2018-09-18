b295c708c761907131f5bbbb72d69924b6da078b
golroz5950 <golroz5950@yahoo.com>
2018-09-18 13:16:22 +0430

    Add files via upload

---
+++ sugarorm/src/main/java/ir/pearly/sugarorm/SugarApp.java
@@ -0,0 +1,21 @@
+package ir.pearly.sugarorm;
+
+
+
+import android.app.Application;
+
+public class SugarApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SugarContext.init(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        SugarContext.terminate();
+    }
+
+}
